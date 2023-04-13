a7450ca95e34140531e2037997c990a4da7e7f6f
4ewka <fanatuk19995@gmail.com>
2023-04-13 20:35:04 +0300

    activity_registration.xml and RegistrationActivity.java add

--- app/src/main/java/com/example/rep/MainActivity.java
+++ app/src/main/java/com/example/rep/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rep;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Здесь можно добавить код, который выполнится при нажатии кнопки
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/rep/RegistrationActivity.java
@@ -0,0 +1,4 @@
+package com.example.rep;
+
+public class RegistrationActivity {
+}
